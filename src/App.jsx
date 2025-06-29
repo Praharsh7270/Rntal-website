28bf2846a56636da1491c87a4c79a9446ef306cb
Praharsh <praharsh_bc22070@srmcem.ac.in>
2025-06-29 18:26:06 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Nav from './Components/Nav/Nav'
+import Home from './Components/Home/Home'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <Nav />
+     <Home />
+    </>
+  )
+}
+
+export default App
